a392edf88bb3cc1c1afeecd5fe494f8b177cee77
Andreas Astrom <andreas.astr@gmail.com>
2016-07-17 21:11:11 +0200

    Changed sorting order

--- hello/activity.py
+++ hello/activity.py
@@ -13,7 +13,7 @@ def get_countdown_activity():
 	startdate = timezone.now() - timezone.timedelta(hours=1)
 	enddate = timezone.now()
 	all_acivities = Countdown.objects.filter(starttime__gt=enddate)
-	all_acivities_orderd = all_acivities.order_by('createdtime')
+	all_acivities_orderd = all_acivities.order_by('starttime')
 	all_acivities_serialized = serializers.serialize('json', all_acivities_orderd)
 	print "Klar"
 	return all_acivities_serialized
