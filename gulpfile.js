1973917cddb408a74d2ffae84329efc5498be551
Andreas Astrom <andreas.astr@gmail.com>
2016-08-05 14:12:30 +0200

    Small changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function () {
+gulp.task('build', function () {
   return gulp.src('hello/static/style/main_style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('hello/static/style/css'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('watch', function () {
   gulp.watch('hello/static/style/scss/**/*.scss', ['sass']);
 });
 
