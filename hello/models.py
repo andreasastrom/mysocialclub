df22e8f3f332c1bc582e57fcadc59c812c55aeb1
Andreas <andreas.astr@gmail.com>
2017-09-01 20:04:08 -0700

    db update

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Greeting(models.Model):
@@ -8,6 +9,7 @@ class Greeting(models.Model):
 class Checklist(models.Model):
 	name = models.CharField(max_length=100)
 	removed = models.IntegerField(default=0)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Item(models.Model):
 	name = models.CharField(max_length=100)
